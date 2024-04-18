453c46f8fd51f628210b51d23d2d9220aa8c6528
j9263178 <j9263178@gmail.com>
2024-04-18 12:27:36 +0800

    update sh

--- run_xxz.sh
+++ run_xxz.sh
@@ -1,1 +1,2 @@
-python examples/kagome/optim_spin1_kagome.py --ansatz IPEPS --bond_dim 2 --chi 20 --GLOBALARGS_device cuda
+#python examples/kagome/optim_spin1_kagome.py --ansatz IPEPS --bond_dim 2 --chi 20 --GLOBALARGS_device cuda
+python optim_spin1_xxz_kagome.py --delta 1 --h 0 --ansatz IPESS --bond_dim 2 --chi 20 --GLOBALARGS_device cuda
